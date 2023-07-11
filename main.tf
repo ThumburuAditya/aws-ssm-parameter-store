263855859452192e538fad0b4d11e7fcdb7bd219
ThumburuAditya <chandramouliadityat@gmail.com>
2023-07-12 02:00:40 +0530

    KMS ID

--- main.tf
+++ main.tf
@@ -1,5 +1,6 @@
 resource "aws_ssm_parameter" "parameter" {
   name  = "test.test"
-  type  = "String"
+  type  = "SecureString"
   value = "Hello World"
+  key_id = "fac9ed94-2498-4257-bc1f-36c4e18ca563"
 }
