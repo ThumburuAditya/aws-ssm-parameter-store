74f5ba89c0383173094c0e8ea302bd189f0bd46a
ThumburuAditya <chandramouliadityat@gmail.com>
2023-07-13 14:56:45 +0530

    parameter store

--- main.tf
+++ main.tf
@@ -1,6 +1,5 @@
 resource "aws_ssm_parameter" "parameter" {
-  name  = "test.test"
-  type  = "SecureString"
-  value = "Hello World"
-  key_id = "fac9ed94-2498-4257-bc1f-36c4e18ca563"
+  name  = "test.test1"
+  type  = "String"
+  value = "Hello world"
 }
