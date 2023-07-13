75df45b74721fa87f07fa46c49f6bf4a80ea8de9
ThumburuAditya <chandramouliadityat@gmail.com>
2023-07-13 16:40:14 +0530

    making the changes

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 resource "aws_ssm_parameter" "parameters" {
   count = length(var.parameters)
   name  = var.parameters[count.index].name
-  type  = var.parameters[count.index].value
-  value = "Hello world"
+  value  = var.parameters[count.index].value
+  type = "String"
   key_id = "fac9ed94-2498-4257-bc1f-36c4e18ca563"
 }
