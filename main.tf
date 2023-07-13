9d36fa8e623fc56430582fe36ae1666e5c53b850
ThumburuAditya <chandramouliadityat@gmail.com>
2023-07-13 16:03:40 +0530

    making the changes

--- main.tf
+++ main.tf
@@ -1,6 +1,7 @@
-resource "aws_ssm_parameter" "parameter" {
-  name  = "test.test1"
-  type  = "SecureString"
+resource "aws_ssm_parameter" "parameters" {
+  count = length(var.parameters)
+  name  = var.parameters[count.index].name
+  type  = var.parameters[count.index].value
   value = "Hello world"
   key_id = "fac9ed94-2498-4257-bc1f-36c4e18ca563"
 }
