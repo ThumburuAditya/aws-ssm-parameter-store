2df8acada1666c221e53320cf652f5d2b35d0507
ThumburuAditya <chandramouliadityat@gmail.com>
2023-07-13 15:10:31 +0530

    Adding KMS Id

--- main.tf
+++ main.tf
@@ -1,5 +1,6 @@
 resource "aws_ssm_parameter" "parameter" {
   name  = "test.test1"
-  type  = "String"
+  type  = "SecureString"
   value = "Hello world"
+  key_id = "fac9ed94-2498-4257-bc1f-36c4e18ca563"
 }
