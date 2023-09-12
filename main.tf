b91d5be993a69a0ddd522ec7cc818819c072974c
ThumburuAditya <chandramouliadityat@gmail.com>
2023-09-12 22:25:09 +0530

    Adding the code

--- main.tf
+++ main.tf
@@ -1,15 +1,15 @@
 resource "aws_ssm_parameter" "parameters" {
-  count = length(var.parameters)
-  name  = var.parameters[count.index].name
+  count  = length(var.parameters)
+  name   = var.parameters[count.index].name
   value  = var.parameters[count.index].value
-  type = "String"
-  key_id = "fac9ed94-2498-4257-bc1f-36c4e18ca563"
+  type   = "String"
+  key_id = "dce90622-5a23-4f82-a639-be841f534702"
 }
 
 resource "aws_ssm_parameter" "passwords" {
-  count = length(var.passwords)
-  name  = var.passwords[count.index].name
+  count  = length(var.passwords)
+  name   = var.passwords[count.index].name
   value  = var.passwords[count.index].value
-  type = "SecureString"
-  key_id = "fac9ed94-2498-4257-bc1f-36c4e18ca563"
+  type   = "SecureString"
+  key_id = "dce90622-5a23-4f82-a639-be841f534702"
 }
